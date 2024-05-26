7770b7c83b2867967e85dd5685a03f08fe32f81a
Gowtham <153164286+Gowtham852k03@users.noreply.github.com>
2024-05-26 23:40:17 +0530

    Update Typed.js

--- src/Components/Typed.js
+++ src/Components/Typed.js
@@ -6,7 +6,7 @@ const Typed = () => {
     <div className="TypeEffect">
       <Typewriter
         options={{
-          strings: ['<b style="color:rgb(160, 103, 250)">Web Developer', '<b style="color: rgb(121, 101, 250)">UI/UX Designer'],
+          strings: ['<b style="color:aliceblue">Web Developer', '<b style="color:aliceblue">UI/UX Designer'],
           autoStart: true,
           loop: true,
 
